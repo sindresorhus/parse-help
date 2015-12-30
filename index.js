45584d6eca9e6c8211da5615ed009278903a8fae
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-30 18:37:33 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+var execall = require('execall');
+var re = /^\s*[^$]\s*(?:-([a-z-]), )?--([a-z-]+) +(.*)$/igm;
+
+module.exports = function (str) {
+	var ret = {
+		flags: {},
+		aliases: {}
+	};
+
+	execall(re, str).forEach(function (x) {
+		var s = x.sub;
+
+		if (s[0]) {
+			ret.aliases[s[0]] = s[1];
+		}
+
+		if (s[1]) {
+			var f = ret.flags[s[1]] = {};
+
+			if (s[0]) {
+				f.alias = s[0];
+			}
+
+			if (s[2]) {
+				f.description = s[2];
+			}
+		}
+	});
+
+	return ret;
+};
---
+++ test.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import fn from './';
+
+const fixture = `
+	Usage
+	  $ unicorn <name>
+
+	Options
+	  --rainbow    Lorem ipsum dolor sit amet
+	  -m, --magic  Aenean commodo ligula eget dolor
+	  --pony       Nullam dictum felis eu pede
+	  -c, --color  Donec quam felis
+
+	Examples
+	  $ unicorn Peachy
+	  $ unicorn Sparkle --rainbow --magic
+`;
+
+test(t => {
+	const x = fn(fixture);
+	t.same(Object.keys(x.flags), ['rainbow', 'magic', 'pony', 'color']);
+	t.is(x.flags.rainbow.description, 'Lorem ipsum dolor sit amet');
+	t.is(x.flags.magic.alias, 'm');
+	t.is(x.aliases.m, 'magic');
+	t.is(x.aliases.c, 'color');
+});
